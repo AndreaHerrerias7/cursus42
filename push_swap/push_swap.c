74aeee8d9582580f719ce0978082cd67c0248e82
Andrea Herrerias - Catedra Chip <andreahl2001@gmail.com>
2025-02-12 13:28:56 +0100

    start push_swap correct, read diferent types of arguments

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,34 +6,77 @@
 /*   By: aherrerias <aherrerias@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 13:28:46 by aherrerias        #+#    #+#             */
-/*   Updated: 2025/02/03 15:24:21 by aherrerias       ###   ########.fr       */
+/*   Updated: 2025/02/12 13:27:46 by aherrerias       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, const char **argv)
+int count_numbers(int argc, char **argv)
 {
-    int 	i;
-	int 	val;
-	char	**a_str;
-	int		*a_int;
+	int count = 0;
+	int i = 1;
+	char **split;
 
-    i = 0;
+	while (i < argc)
+	{
+		if (ft_strchr(argv[i], ' '))
+		{
+			split = ft_split(argv[i], ' ');
+			for (int j = 0; split[j]; j++)
+				count++;
+		}
+		else
+			count++;
+		i++;
+	}
+	return count;
+}
+
+void fill_stack(int *stack_a, int argc, char **argv)
+{
+	int val;
+	int j;
+	int	i;
+	char **a_str;
+
 	val = 1;
-	
-	while(val < argc)
+	j = 0;
+	while (val < argc)
 	{
 		if (ft_strchr(argv[val], ' '))
 		{
+			i = 0;
 			a_str = ft_split(argv[val], ' ');
+			while (a_str[i])
+			{
+				stack_a[j++] = ft_atoi(a_str[i]);
+				i++;
+			}
 		}
+		else
+			stack_a[j++] = ft_atoi(argv[val]);
 		val++;
 	}
-	while (a_str[i])
+	stack_a[j] = 0;
+}
+
+int main(int argc, char **argv)
+{
+	int *stack_a;
+	int total_numbers = count_numbers(argc, argv);
+	int j;
+
+	stack_a = malloc(sizeof(int) * (total_numbers + 1));
+	if (!stack_a)
+		return (0);
+
+	fill_stack(stack_a, argc, argv);
+	j = 0;
+	while (stack_a[j])
 	{
-		a_int[i] = ft_atoi(a_str[i]);
-		i++;
+		printf("%i ", stack_a[j]);
+		j++;
 	}
-	printf("a_int %i", a_int[5]);
+	return (0);
 }
